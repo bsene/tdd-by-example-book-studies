c9f49d62af9bc68e9eded8ebf2eb05dd9aeac12b
Birame SENE <bsene@pm.me>
2022-03-01 23:57:15 +0100

    refactoring

--- dollar.sh
+++
@@ -1,6 +0,0 @@
-
-dollar__times() {
-    amount=$1
-    multiplier=$2
-    echo $(($amount * $multiplier))
-}
--- dollar_test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-source "$(pwd)/dollar.sh"
-
-test_multiplication() {
-    five=5
-    result="$(dollar__times five 2)"
-    assertion__equal 10 $result
-}
