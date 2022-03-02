586c7c6ec276c421b2cf7dda6c36d1b888ed05fa
Birame SENE <bsene@pm.me>
2022-03-02 08:25:44 +0100

    Finished Chapter 2

--- Gemfile
+++ Gemfile
@@ -7,3 +7,5 @@ git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 # gem "rails"
 
 gem "rspec", "~> 3.10"
+
+gem "rufo", "~> 0.13.0", :group => :dev
--- spec/dollar_spec.rb
+++ spec/dollar_spec.rb
@@ -1,11 +1,11 @@
-require 'dollar'
+require "dollar"
 
 RSpec.describe Dollar, "Dollar tests suite" do
-  context "testing multiplication" do
-    it "should return result when multiply by a positive integer" do
-      five = Dollar.new(5)
-      five.times(2)
-      expect(five.amount).to eq 10
-    end
+  it "test multiplication" do
+    five = Dollar.new(5)
+    product = five.times(2)
+    expect(product.amount).to eq 10
+    product = five.times(3)
+    expect(product.amount).to eq 15
   end
 end
